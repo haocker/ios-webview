83c479cefd92ee88d7255230ae915a953c015bf5
haocker <32011015+haocker@users.noreply.github.com>
2025-04-20 11:15:57 +0000

    test loadhtml

--- web-view/ViewController.swift
+++ web-view/ViewController.swift
@@ -15,15 +15,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // 加载本地HTML文件
-        // if let htmlPath = Bundle.main.path(forResource: "www/index", ofType: "html") {
-        //     let url = URL(fileURLWithPath: htmlPath)
-        //     let request = URLRequest(url: url)
-        //     webView.load(request)
-        // } else {
-        //     print("无法找到本地HTML文件")
-        // }
+        //Do any additional setup after loading the view, typically from a nib.
+        //加载本地HTML文件
+        if let htmlPath = Bundle.main.path(forResource: "www/index", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        } else {
+            print("无法找到本地HTML文件")
+        }
         
         // // 设置WebView为全屏，但不隐藏状态栏
         // webView.frame = self.view.bounds
