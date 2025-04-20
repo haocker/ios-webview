cc285e4cc63a6af17121c89010e62cc93cb808cb
haocker <32011015+haocker@users.noreply.github.com>
2025-04-20 11:12:01 +0000

    test

--- web-view/ViewController.swift
+++ web-view/ViewController.swift
@@ -17,21 +17,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // 加载本地HTML文件
-        if let htmlPath = Bundle.main.path(forResource: "www/index", ofType: "html") {
-            let url = URL(fileURLWithPath: htmlPath)
-            let request = URLRequest(url: url)
-            webView.load(request)
-        } else {
-            print("无法找到本地HTML文件")
-        }
+        // if let htmlPath = Bundle.main.path(forResource: "www/index", ofType: "html") {
+        //     let url = URL(fileURLWithPath: htmlPath)
+        //     let request = URLRequest(url: url)
+        //     webView.load(request)
+        // } else {
+        //     print("无法找到本地HTML文件")
+        // }
         
-        // 设置WebView为全屏，但不隐藏状态栏
-        webView.frame = self.view.bounds
+        // // 设置WebView为全屏，但不隐藏状态栏
+        // webView.frame = self.view.bounds
         
-        // 获取状态栏高度并传递给WebView
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        let jsString = "setStatusBarHeight(\(statusBarHeight));"
-        webView.evaluateJavaScript(jsString, completionHandler: nil)
+        // // 获取状态栏高度并传递给WebView
+        // let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        // let jsString = "setStatusBarHeight(\(statusBarHeight));"
+        // webView.evaluateJavaScript(jsString, completionHandler: nil)
     }
     
     override func didReceiveMemoryWarning() {
