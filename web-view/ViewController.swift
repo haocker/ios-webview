930996967ccec4e2f4cc589e6eeae41603782a99
haocker <32011015+haocker@users.noreply.github.com>
2025-04-20 12:27:51 +0000

    继续测试闪退

--- web-view/ViewController.swift
+++ web-view/ViewController.swift
@@ -11,12 +11,19 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     
-    @IBOutlet weak var webView: WKWebView!
+    var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Do any additional setup after loading the view, typically from a nib.
         //加载本地HTML文件
+        // 以编程方式创建和初始化WKWebView
+        let configuration = WKWebViewConfiguration()
+        webView = WKWebView(frame: self.view.bounds, configuration: configuration)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(webView)
+        
+        // 加载本地HTML文件
         if let htmlPath = Bundle.main.path(forResource: "www/index", ofType: "html") {
             let url = URL(fileURLWithPath: htmlPath)
             let request = URLRequest(url: url)
@@ -25,27 +32,18 @@ class ViewController: UIViewController, WKNavigationDelegate {
             print("无法找到本地HTML文件")
         }
         
-        // // 设置WebView为全屏，但不隐藏状态栏
-        webView.frame = self.view.bounds
-        
-        // 设置导航代理
-        webView.navigationDelegate = self
-        
-        // 获取状态栏高度并传递给WebView
-        // 由于页面可能尚未加载完成，我们在didFinishNavigation中执行JavaScript
+        // 使用计时器延迟执行JavaScript代码
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let statusBarHeight = UIApplication.shared.statusBarFrame.height
+            let jsString = "setStatusBarHeight(\(statusBarHeight));"
+            self.webView.evaluateJavaScript(jsString, completionHandler: nil)
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
-        // MARK: - WKNavigationDelegate
-        
-        //func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            // let statusBarHeight = UIApplication.shared.statusBarFrame.height
-            // let jsString = "setStatusBarHeight(\(statusBarHeight));"
-            // webView.evaluateJavaScript(jsString, completionHandler: nil)
-        //}
     }
     
     
